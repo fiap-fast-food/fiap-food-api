c479501461d21f86cf301019952d3f8413ad8df6
guizin9 <guipe7@hotmail.com>
2024-07-14 20:57:37 -0300

    Demonstration of CI/CD pipeline and deploy

--- src/main/java/com/fiap/food/client/dto/PaymentRequestClientDTO.java
+++ src/main/java/com/fiap/food/client/dto/PaymentRequestClientDTO.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class PaymentRequestClientDTO {
+    // teste deploy
     private Double amount;
     private LocalDateTime timestamp;
     private String status;
